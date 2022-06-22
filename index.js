32bf4be553db6bb44e978d7a014b518ef511340e
MikyFit <mikyfiteu@gmail.com>
2022-06-22 13:21:09 +0200

    Export values into *.csv file
    The fs.writeFile() method is used to asynchronously write the specified data to a file.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fs = require("fs/promises");
 
 // Get current tokens prices from api.binance.com
 const get_binance_pair_price = async (pair) => {
@@ -16,10 +17,29 @@ const get_binance_pair_price = async (pair) => {
         const BTCUSDT = await get_binance_pair_price("BTCUSDT")
         const btcPrice = parseFloat(BTCUSDT['price'])
 
+        const ethUSDT = await get_binance_pair_price("ETHUSDT")
+        const ethPrice = parseFloat(ethUSDT['price'])
+
+        const FTMUSDT = await get_binance_pair_price("FTMUSDT")
+        const ftmPrice = parseFloat(FTMUSDT['price'])
+
+        const BNBUSDT = await get_binance_pair_price("BNBUSDT")
+        const bnbPrice = parseFloat(BNBUSDT['price'])
+
+        const AVAXUSDT = await get_binance_pair_price("AVAXUSDT")
+        const avaxPrice = parseFloat(AVAXUSDT['price'])
+
         console.log('BTC Price:', '$' + btcPrice,)
+        console.log('ETH Price:', '$' + ethPrice,)
+        console.log('FTM Price:', '$' + ftmPrice,)
+        console.log('BNB Price:', '$' + bnbPrice,)
+        console.log('AVAX Price:', '$' + avaxPrice,)
         console.log('==>Scraping from Binance API finished');
         console.log('');
 
+        await fs.writeFile('prices.csv', 'BTC Price' + ';' + 'ETH Price' + ';' + 'FTM Price' + ';' + 'BNB Price' + ';' + 'AVAX Price' + '\n' + btcPrice + ';' + ethPrice + ';' + ftmPrice + ';' + bnbPrice + ';' + avaxPrice, function (err) {
+            if (err) throw err;
+        });
         // Closing app
         process.exit(0)
     } catch (error) {
